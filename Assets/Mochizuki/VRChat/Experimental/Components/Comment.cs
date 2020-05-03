c02b6229a0fe468a5b18ed81af51daff796c0b0f
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-05-03 17:19:22 +0900

    feat: working?

---
+++ Assets/Mochizuki/VRChat/Experimental/Components/Comment.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Mochizuki.VRChat.Experimental.Components
+{
+    public class Comment : MonoBehaviour
+    {
+        [SerializeField]
+        [TextArea]
+        private string Notes = "";
+    }
+}
